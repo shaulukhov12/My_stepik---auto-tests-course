78e770dfebe253cc85e1f5139d611251aba2330b
shaulukhov12 <shaulukhov12@gmail.com>
2020-06-16 12:34:55 +0300

    add smth

--- Class3_Selenium/Section3/Lesson1.py
+++ Class3_Selenium/Section3/Lesson1.py
@@ -1,2 +1,13 @@
 assert abs(-42)== -42, "Должно быть абсолютное значение" #Ghjgbcsdfnm ошибки подобным родом, чтобы легче было понять.
 #Если к примеру выполняется поиск элемента на странице то нужно к assert явно прописывать о каком элементе идет речь.
+
+
+#Форматирование строк с помощью str.format:
+#"Let's count together: {}, then goes {}, and then {}".format("one", "two", "three")
+
+#F-strings
+
+#str1 = "one"
+#str2 = "two"
+#str3 = "three"
+#print(f"Let's count together: {str1}, then goes {str2}, and then {str3}")
