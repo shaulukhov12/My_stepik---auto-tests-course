a069b79348ec7199b0c57c98f1cb3db8994354c2
shaulukhov12 <shaulukhov12@gmail.com>
2020-06-17 17:32:05 +0300

    pytest

--- Class3_Selenium/Section3/test_task3.py
+++ Class3_Selenium/Section3/test_task3.py
@@ -5,7 +5,7 @@ import unittest
 
 class Test(unittest.TestCase):
     def test_reg(self):
-        link = "http://suninjuly.github.io/registration2.html"
+        link = "http://suninjuly.github.io/registration1.html"
         browser = webdriver.Chrome()
         browser.get(link)
 
