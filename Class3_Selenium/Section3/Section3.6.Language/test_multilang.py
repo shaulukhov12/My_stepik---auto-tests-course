5bf2270914fd96dabd520b845df36b84c9e19a05
shaulukhov12 <shaulukhov12@gmail.com>
2020-06-18 20:00:33 +0300

    add

---
+++ Class3_Selenium/Section3/Section3.6.Language/test_multilang.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+browser = webdriver.Chrome(options=options)
+
+
+Для Firefox:
+fp = webdriver.FirefoxProfile()
+fp.set_preference("intl.accept_languages", user_language)
+browser = webdriver.Firefox(firefox_profile=fp)
